37a1e312a33e24371ba798fa165beb6b0cab47dd
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-26 11:13:23 +0200

    refacto var fix case

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -2,11 +2,11 @@
 public class TennisGame2 implements TennisGame {
     public static final String SEPARATOR = "-";
     public static final String ALL = "All";
-    public int P1point = 0;
-    public int P2point = 0;
+    public int p1point = 0;
+    public int p2point = 0;
 
-    public String P1res = "";
-    public String P2res = "";
+    public String p1res = "";
+    public String p2res = "";
     private String player1Name;
     private String player2Name;
 
@@ -17,61 +17,61 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if ( P1point == P2point && P1point < 4 ) {
-            score = getSuitableMessage (P1point);
+        if ( p1point == p2point && p1point < 4 ) {
+            score = getSuitableMessage (p1point);
             score += SEPARATOR + ALL;
         }
-        if ( P1point == P2point && P1point >= 3 )
+        if ( p1point == p2point && p1point >= 3 )
             score = TennisScore.DEUCE.getScore ( );
 
-        if ( P1point > 0 && P2point == 0 ) {
-            P1res = getSuitableMessage (P1point);
+        if ( p1point > 0 && p2point == 0 ) {
+            p1res = getSuitableMessage (p1point);
 
-            P2res = TennisScore.LOVE.getScore ( );
-            score = P1res + SEPARATOR + P2res;
+            p2res = TennisScore.LOVE.getScore ( );
+            score = p1res + SEPARATOR + p2res;
         }
-        if ( P2point > 0 && P1point == 0 ) {
-            P2res = getSuitableMessage (P2point);
+        if ( p2point > 0 && p1point == 0 ) {
+            p2res = getSuitableMessage (p2point);
 
-            P1res = TennisScore.LOVE.getScore ( );
-            score = P1res + SEPARATOR + P2res;
-        }
-
-        if ( P1point > P2point && P1point < 4 ) {
-            if ( P1point == 2 )
-                P1res = TennisScore.THIRTY.getScore ( );
-            if ( P1point == 3 )
-                P1res = TennisScore.FORTY.getScore ( );
-            if ( P2point == 1 )
-                P2res = TennisScore.FIFTEEN.getScore ( );
-            if ( P2point == 2 )
-                P2res = TennisScore.THIRTY.getScore ( );
-            score = P1res + SEPARATOR + P2res;
-        }
-        if ( P2point > P1point && P2point < 4 ) {
-            if ( P2point == 2 )
-                P2res = TennisScore.THIRTY.getScore ( );
-            if ( P2point == 3 )
-                P2res = TennisScore.FORTY.getScore ( );
-            if ( P1point == 1 )
-                P1res = TennisScore.FIFTEEN.getScore ( );
-            if ( P1point == 2 )
-                P1res = TennisScore.THIRTY.getScore ( );
-            score = P1res + SEPARATOR + P2res;
+            p1res = TennisScore.LOVE.getScore ( );
+            score = p1res + SEPARATOR + p2res;
         }
 
-        if ( P1point > P2point && P2point >= 3 ) {
+        if ( p1point > p2point && p1point < 4 ) {
+            if ( p1point == 2 )
+                p1res = TennisScore.THIRTY.getScore ( );
+            if ( p1point == 3 )
+                p1res = TennisScore.FORTY.getScore ( );
+            if ( p2point == 1 )
+                p2res = TennisScore.FIFTEEN.getScore ( );
+            if ( p2point == 2 )
+                p2res = TennisScore.THIRTY.getScore ( );
+            score = p1res + SEPARATOR + p2res;
+        }
+        if ( p2point > p1point && p2point < 4 ) {
+            if ( p2point == 2 )
+                p2res = TennisScore.THIRTY.getScore ( );
+            if ( p2point == 3 )
+                p2res = TennisScore.FORTY.getScore ( );
+            if ( p1point == 1 )
+                p1res = TennisScore.FIFTEEN.getScore ( );
+            if ( p1point == 2 )
+                p1res = TennisScore.THIRTY.getScore ( );
+            score = p1res + SEPARATOR + p2res;
+        }
+
+        if ( p1point > p2point && p2point >= 3 ) {
             score = "Advantage " + player1Name;
         }
 
-        if ( P2point > P1point && P1point >= 3 ) {
+        if ( p2point > p1point && p1point >= 3 ) {
             score = "Advantage " + player2Name;
         }
 
-        if ( P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2 ) {
+        if ( p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2 ) {
             score = "Win for player1";
         }
-        if ( P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2 ) {
+        if ( p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2 ) {
             score = "Win for player2";
         }
         return score;
@@ -89,11 +89,11 @@ public class TennisGame2 implements TennisGame {
     }
 
     public void P1Score() {
-        P1point++;
+        p1point++;
     }
 
     public void P2Score() {
-        P2point++;
+        p2point++;
     }
 
     public void wonPoint(String player) {
