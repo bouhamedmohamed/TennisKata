dbafd954b9c87b5d3db3d03cadf3b30225c12804
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-26 13:33:49 +0200

    refacto clean code

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,6 +1,8 @@
 
 public class TennisGame3 implements TennisGame {
 
+    public static final String SEPARATOR = "-";
+    public static final String ALL = "All";
     private int pointPlayerTwo;
     private int pointPlayerOne;
     private String playerOneName;
@@ -16,14 +18,18 @@ public class TennisGame3 implements TennisGame {
         if ( pointPlayerOne < 4 && pointPlayerTwo < 4 && !(pointPlayerOne + pointPlayerTwo == 6) ) {
             final String scoreLabelPlayerOne = TennisScore.getScore (pointPlayerOne).getScoreLabel ( );
             final String scoreLabelPlayerTwo = TennisScore.getScore (pointPlayerTwo).getScoreLabel ( );
-            return (pointPlayerOne == pointPlayerTwo) ? scoreLabelPlayerOne + "-All" : scoreLabelPlayerOne + "-" + scoreLabelPlayerTwo;
+            return (pointPlayerOne == pointPlayerTwo) ? scoreLabelPlayerOne + SEPARATOR + ALL : scoreLabelPlayerOne + SEPARATOR + scoreLabelPlayerTwo;
         } else {
             if ( pointPlayerOne == pointPlayerTwo )
-                return "Deuce";
-            String inHead = pointPlayerOne > pointPlayerTwo ? playerOneName : playerTwoName;
-            final int difference = pointPlayerOne - pointPlayerTwo;
-            return (Math.abs (difference) == 1) ? "Advantage " + inHead : "Win for " + inHead;
+                return TennisScore.DEUCE.getScoreLabel ( );
+            return getAdvantagedOrWinner ( );
         }
+    }
+
+    private String getAdvantagedOrWinner() {
+        String playerInHead = pointPlayerOne > pointPlayerTwo ? playerOneName : playerTwoName;
+        final int difference = pointPlayerOne - pointPlayerTwo;
+        return (Math.abs (difference) == 1) ? "Advantage " + playerInHead : "Win for " + playerInHead;
     }
 
     public void wonPoint(String playerName) {
